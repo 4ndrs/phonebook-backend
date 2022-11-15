b6e61368df572488722ff4643a01c1aa020ca6a5
Andres Eloy Rivera Garcia <andres.degozaru@gmail.com>
2022-11-15 17:20:56 +0100

    Get the port definition from process.env.PORT

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const morgan = require("morgan");
 
 const app = express();
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 const morugan = (tokens, request, response) => {
   const method = tokens.method(request, response);
